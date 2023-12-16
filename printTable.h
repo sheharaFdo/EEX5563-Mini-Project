71c6c3adcb4c9c9e45f2f8a87dab97270e307706
sheharaFdo <138082669+sheharaFdo@users.noreply.github.com>
2023-12-16 11:44:07 +0530

    Create printTable.h

---
+++ printTable.h
@@ -0,0 +1,30 @@
+#ifndef __PRINT__TABLE__
+#define __PRINT__TABLE__
+
+// Print Table
+
+#include "./Process.h"
+
+/**
+ * print_table 
+ */
+void print_table(Process p[], int n)
+{
+	int i;
+
+	puts("\t+-----+------------+-------------+----------+-------------+-----------------+--------------+-----------------+");
+	puts("\t| PID | Burst Time | Arrive Time | Priority | Return Time |  Response Time  | Waiting Time | Turnaround Time |");
+	puts("\t+-----+------------+-------------+----------+-------------+-----------------+--------------+-----------------+");
+
+	for (i = 0; i < n; i++)
+	{
+		printf("\t| %3s |     %3d    |     %3d     |    %3d   |     %3d     |      %3d        |      %3d     |        %3d      |\n",
+			p[i].id, p[i].burst, p[i].arrive_time, p[i].priority, p[i].return_time, p[i].response_time, p[i].waiting_time, p[i].turnaround_time);
+
+		puts("\t+-----+------------+-------------+----------+-------------+-----------------+--------------+-----------------+");
+	}
+
+	puts("\n");
+}
+
+#endif
